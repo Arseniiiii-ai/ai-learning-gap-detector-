ff59e3ddfc018c072d7a85e3398707521deb91c5
Arsen <baktygalievarsen431@gmail.com>
2026-03-23 12:28:07 +0500

    Delete learning-gap-ai/dashboard/learning-gap-ai directory

--- learning-gap-ai/dashboard/learning-gap-ai/src/tensor_builder.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def build_tensor(encoded_sequences):
-    tensor_data = []
-    for student, seq in encoded_sequences.items():
-        tensor_seq = torch.tensor(seq, dtype=torch.float32)
-        tensor_data.append(tensor_seq)
-    return tensor_data
